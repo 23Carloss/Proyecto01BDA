89d60dccef2abd81e89f9458e69472910ce0f365
Carloss <luiscmg06@outlook.es>
2025-06-13 07:59:52 -0700

    Update PersistenciaException.java
    agregue el metodo nomas

--- ProyectoUnidadUnoPrestamos/src/main/java/Persistencia/PersistenciaException.java
+++ ProyectoUnidadUnoPrestamos/src/main/java/Persistencia/PersistenciaException.java
@@ -1,4 +1,4 @@
-/*
+enfs/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -8,6 +8,9 @@ package Persistencia;
  *
  * @author Sandra
  */
-public class PersistenciaException {
+public class PersistenciaException extends Exception{
+    public PersistenciaException(String m){
+        super(m);
+    }
     
 }
