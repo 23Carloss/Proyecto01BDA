46a8f5868e70262765aac67b0862ea8c3223ac39
Jonathan Romero <109704905+yehodarlyn@users.noreply.github.com>
2025-06-16 21:27:24 -0700

    Update Prestamo.java

--- ProyectoUnidadUnoPrestamos/src/main/java/Dominio/Prestamo.java
+++ ProyectoUnidadUnoPrestamos/src/main/java/Dominio/Prestamo.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package Dominio;
 
 /**
@@ -10,5 +9,34 @@ package Dominio;
  * @author $Luis Carlos Manjarrez Gonzalez
  */
 public class Prestamo {
+
+    private int id;
+    private Double adeudo;
+
+    public Prestamo(int id, Double adeudo) {
+        this.id = id;
+        this.adeudo = adeudo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getAdeudo() {
+        return adeudo;
+    }
+
+    public void setAdeudo(Double adeudo) {
+        this.adeudo = adeudo;
+    }
+
+    @Override
+    public String toString() {
+        return "Prestamo{" + "id=" + id + ", adeudo=" + adeudo + '}';
+    }
 
 }
